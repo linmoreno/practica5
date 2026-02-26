ff086dfd452e0da3d17c77324a187e7784a19b9c
linmoreno <linda.moreno216690@potros.itson.edu.mx>
2026-02-25 17:48:55 -0700

    practica terminada

---
+++ app/src/main/java/saldana/ruben/practica5/Pelicula.kt
@@ -0,0 +1,6 @@
+package saldana.ruben.practica5
+
+data class Pelicula(var titulo:String,
+                                         var image:Int,
+                                         var header:Int,
+                                         var sinopsis:String)
