8085b578b8f8de0bae55169ef7623dc665bf130b
linmoreno <linda.moreno216690@potros.itson.edu.mx>
2026-05-13 13:30:21 -0700

    practica6

--- app/src/main/java/saldana/ruben/practica5/detallePelicula.kt
+++ app/src/main/java/saldana/ruben/practica5/detallePelicula.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
 class detallePelicula : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/saldana/ruben/practica5/seatSelection.kt
+++ app/src/main/java/saldana/ruben/practica5/seatSelection.kt
@@ -1,26 +1,16 @@
 package saldana.ruben.practica5
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class seatSelection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val button : Button = findViewById(R.id.botonP)
-
-        button.setOnClickListener (){
-            var intent = Intent(this, Catalogo::class.java)
-            startActivity(intent)
-        }
-
+        setContentView(R.layout.activity_seat_selection)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
